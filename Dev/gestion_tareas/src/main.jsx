7b984e373edb2ec2771056c82d76df46a8f9a7b5
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-02-21 16:13:57 -0500

    Logo, Paleta de colores y rutas

--- Dev/gestion_tareas/src/main.jsx
+++ Dev/gestion_tareas/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 import Header from './components/header/header.jsx'
@@ -8,6 +9,11 @@ import Header from './components/header/header.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
