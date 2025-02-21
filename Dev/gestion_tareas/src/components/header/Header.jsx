0f3a31c4d77e85cdbd2451412d4bcbe444e6ac69
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-02-21 10:22:14 -0500

    Header falta logo, color, tipografía

---
+++ Dev/gestion_tareas/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+function Header() {
+    return (
+        <header className="header">
+            <h2>LOGO</h2>
+            <h2>APP</h2>
+        </header>
+    );
+}
+
+export default Header;
