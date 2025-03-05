7e8d1cf46a18263b433e56379972d04c307b175f
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-03-04 20:34:32 -0500

    Arreglo de creación de tarea con id

--- Dev/gestion_tareas/src/methods/crearTarea.js
+++ Dev/gestion_tareas/src/methods/crearTarea.js
@@ -1,16 +1,25 @@
 function crearTarea(data){
     const proyectos = JSON.parse(localStorage.getItem('proyectos'));
+    let ultimoTareaID = localStorage.getItem('tareaID');
+    if(!ultimoTareaID){
+        ultimoTareaID = 1;
+        localStorage.setItem('tareaID', ultimoTareaID);
+    } else {
+        ultimoTareaID = parseInt(ultimoTareaID);
+    }
     const proyectoIndex = proyectos.findIndex(
         proyecto => proyecto.id == data.proyectoID && proyecto.estado == 'activo');
     if(proyectoIndex != -1){
         proyectos[proyectoIndex].tareas.push(
             {
+                "id": ultimoTareaID + 1,
                 "titulo": data.titulo,
                 "descripcion": data.descripcion,
                 "fecha": data.fecha,
                 "estado": "activo"
             }
         );
+        localStorage.setItem('tareaID', ultimoTareaID + 1);
         localStorage.setItem('proyectos', JSON.stringify(proyectos));
     }
 }
--- Dev/gestion_tareas/src/methods/traerProyectos.js
+++ Dev/gestion_tareas/src/methods/traerProyectos.js
@@ -1,5 +1,19 @@
 function traerProyectos(){
     const proyectos = JSON.parse(localStorage.getItem('proyectos'));
+    if(!proyectos){
+        localStorage.setItem('proyectos', JSON.stringify(
+            [
+                {
+                    "id": 1,
+                    "titulo": "Predeterminado",
+                    "descripcion": "Proyecto predeterminado",
+                    "fecha": null,
+                    "tareas": [],
+                    "estado": "activo"
+                }
+            ]
+        ))
+    }
     const proyectoPendientes = proyectos.filter(
         proyecto => proyecto.estado === 'activo');
     const proyectosCompletados = proyectos.filter(
