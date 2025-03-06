434737ee4412b785d7e3580c7a973e22cc628ec0
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-03-06 13:00:38 -0500

    Barra de progreso para los proyectos, falta arreglar estilos

--- Dev/gestion_tareas/src/pages/home/Home.jsx
+++ Dev/gestion_tareas/src/pages/home/Home.jsx
@@ -11,9 +11,16 @@ function Home() {
     useEffect(() => {
         const tareas = traerTareas();
         setTareas(tareas[0].tareas);
-        setProyectos(traerProyectos()[0].proyectos);
-        console.log("tareasRef.current:", tareasRef.current);
+        var totalTareas, tareasCompletadas, progreso;
+        const proyectoProgreso = traerProyectos()[0].proyectos.map(proyecto => {
+            totalTareas = proyecto.tareas.length;
+            tareasCompletadas = proyecto.tareas.filter(tarea => tarea.estado == "completado").length;
+            progreso = totalTareas > 0 ? (tareasCompletadas/totalTareas)*100 : 0;
+            return {...proyecto, progreso }
+        })
+        setProyectos(proyectoProgreso);
     }, []);
+    //Para slider de tareas
     const scroll = (ref, direction) => {
         if (ref.current) {
             console.log("Scroll amount:", direction === 'left' ? -150 : 150);
@@ -24,13 +31,13 @@ function Home() {
             console.log("⚠ ref.current es null o undefined");
         }
     };
+
     return (
         <div className="contenedor">
             <span />
             <h2>Inicio</h2>
             <p className="subtitulo">Tareas pendientes</p>
             <div className="tareasInicio">
-                {/*<button className="boton-navegacion">‹</button>*/}
                 <button className="boton-navegacion izquierda" onClick={() => scroll(tareasRef, 'left')}>{"<"}</button>
                 <div className="seccion" ref={tareasRef}>
                     {tareas.map((tarea, index) => (
@@ -41,13 +48,16 @@ function Home() {
                     ))}
                 </div>
                 <button className="boton-navegacion derecha" onClick={() => scroll(tareasRef, 'right')}>{">"}</button>
-                {/*<button className="boton-navegacion">›</button>*/}
             </div>
             <p className="subtitulo">Proyectos pendientes</p>
             <div className="proyectosInicio">
                 {proyectos.map((proyecto) => (
                     <div className="contenedor_proyecto" key={proyecto.id}>
                         <p>{proyecto.titulo} {proyecto.fecha}</p>
+                        <div className="contenedor_progreso">
+                            <div className="barra_progreso" style={{ width: `${proyecto.progreso}%`}} />
+                            <span>{proyecto.progreso}%</span>
+                        </div>
                     </div>
                 ))}
             </div>
