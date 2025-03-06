6fdf379a833ae32944d68f9c3145a8b8b47451a4
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-03-05 21:48:46 -0500

    scroll horizontal no funciona

--- Dev/gestion_tareas/src/pages/home/Home.jsx
+++ Dev/gestion_tareas/src/pages/home/Home.jsx
@@ -1,14 +1,25 @@
 import "./Home.scss";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import traerTareas from "../../methods/traerTareas";
+import traerProyectos from "../../methods/traerProyectos"
 
 function Home() {
     const [tareas, setTareas] = useState([]);
+    const [proyectos, setProyectos] = useState([]);
+    const tareasRef = useRef(null);
 
     useEffect(() => {
         const tareas = traerTareas();
         setTareas(tareas[0].tareas);
+        setProyectos(traerProyectos()[0].proyectos);
     }, []);
+
+    const scroll = (ref, direccion) => {
+        if(ref.current){
+            const scrollAmount = direccion == 'left' ? -200 : 200
+            ref.current.scrollLeft += scrollAmount;
+        }
+    };
     
     return (
         <div className="contenedor">
@@ -16,10 +27,20 @@ function Home() {
             <h2>Inicio</h2>
             <p className="subtitulo">Tareas pendientes</p>
             <div className="tareasInicio">
+            <button className="boton-navegacion izquierda" onClick={() => scroll(tareasRef, 'left')}>{"<"}</button>
                 {tareas.map((tarea, index) => (
                     <div key={index} className="contenedor_tarea">
                         <p>{tarea.titulo}</p>
                         <p>{tarea.fecha}</p>
+                    </div>
+                ))}
+                <button className="boton-navegacion derecha" onClick={() => scroll(tareasRef, 'right')}>{">"}</button>
+            </div>
+            <p className="subtitulo">Proyectos pendientes</p>
+            <div className="proyectosInicio">
+                {proyectos.map((proyecto) => (
+                    <div className="contenedor_proyecto" key={proyecto.id}>
+                        <p>{proyecto.titulo}</p>
                     </div>
                 ))}
             </div>
