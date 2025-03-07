372fe7012eb2cb44448e6adcac5b1769bf601806
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-03-06 20:52:48 -0500

    animacion al cambiar de ruta y arreglo de ubicacion boton agregar

--- Dev/gestion_tareas/src/main.jsx
+++ Dev/gestion_tareas/src/main.jsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
+import { StrictMode, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router"
 import './index.css'
 import Home from './pages/home/Home.jsx'
 import Tareas from './pages/tareas/Tareas.jsx'
@@ -11,21 +11,43 @@ import Crear_Proyecto from './pages/crear_proyecto/Crear_proyecto.jsx'
 import Editar_Proyecto from './pages/editar_proyecto/Editar_proyecto.jsx'
 import Header from './components/header/header.jsx'
 import Menu from './components/menu/Menu.jsx'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import 'animate.css';
+import { node } from 'globals'
 
+function RutasAnimadas(){
+  const locacion = useLocation();
+  const nodeRef = useRef(null)
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={locacion.key}
+        className="animate__animated animate__fadeIn"
+        timeout={300}
+        nodeRef={nodeRef}
+      >
+        <div ref={nodeRef}>
+          <Routes location={locacion}>
+            <Route path="/" element={<Home />} />
+            <Route path="/tareas" element={<Tareas />} />
+            <Route path="/proyectos" element={<Proyectos />} />
+            <Route path="/tareas/crear" element={<Crear_Tarea />} />
+            <Route path="/proyectos/crear" element={<Crear_Proyecto />} />
+            <Route path="/tareas/editar/:proyectoId/:tareaId" element={<Editar_Tarea />} />
+            <Route path="/proyectos/editar/:id" element={<Editar_Proyecto />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
     <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tareas" element={<Tareas />} />
-          <Route path="/proyectos" element={<Proyectos />} />
-          <Route path="/tareas/crear" element={<Crear_Tarea />} />
-          <Route path="/proyectos/crear" element={<Crear_Proyecto />} />
-          <Route path="/tareas/editar/:proyectoId/:tareaId" element={<Editar_Tarea />} />
-          <Route path="/proyectos/editar/:id" element={<Editar_Proyecto />} />
-        </Routes>
+        <RutasAnimadas />
         <Menu/>
     </BrowserRouter>
   </StrictMode>,
